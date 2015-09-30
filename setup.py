07cd2eb4f2d1d26fbeee8f366b072a6f8ae0c2ad
Alexandar Tankov <AlexandarTankov@gmail.com>
2015-10-01 00:18:02 +0300

    Add setup.py and req.txt

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Travelling to Vratsa/Sofia",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=['facebook-sdk==0.4.0'],
+    author="Alexnadar Tankov",
+    author_email="AlexandarTankov@gmali.com",
+    description="This project is created with educational purpose and help finding people for travelling to Vratsa/Sofia",
+    license="MIT",
+    keywords="travel, Vraca, Sofia, travelling",
+    url="https://github.com/AlexanderTankov/Travelling-to-Vratsa-Sofia"
+)
